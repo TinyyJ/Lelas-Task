9ee0ccd282246710c84f05f8c4dbd4e0c7c323c2
TinyyJ <janashvilitiny@gmail.com>
2024-11-27 22:22:14 +0400

    first commit

--- src/main/java/org/example/Main2.java
+++ src/main/java/org/example/Main2.java
@@ -1,4 +1,5 @@
 package org.example;
+
 public class Main2 {
     public static void main(String[] args) {
 
--- src/main/java/org/example/Main3.java
+++ src/main/java/org/example/Main3.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main3 {
     public static void main(String[] args) {
+
         int number = 5;
         if (number >0) {
             System.out.println("დადებითი");
